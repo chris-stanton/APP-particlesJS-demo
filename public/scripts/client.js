1ef610fc9344c600ff0a32ccdc7d801aa5889f71
Chris Stanton <cstanton0760@yahoo.com>
2017-08-06 22:45:54 -0500

    completed fixed values

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,86 @@
 
 console.log("client.js loaded...");
 
-particlesJS.load('particles-js', '../particles.json', function(){
-  console.log('particles.json loaded...');
-  console.log(particles.number);
-});
+/////////////////////////////////////////////////////////////////////
+// // use code below if particles is being sourced from json file. //
+// // will need to run on server if used                           //
+/////////////////////////////////////////////////////////////////////
+
+// particlesJS.load('particles-js', '../particles.json', function(){
+//   console.log('particles.json loaded...');
+// });
 
 
-
-function link_color(){
-  var color = document.getElementById('select').value;
-  console.log(color);
-};
+// particlesJS json
+particlesJS('particles-js',
+   app = {
+    "particles":{
+      "number":{
+        "value":500
+      },
+      "color":{
+        "value":"#3fff04"
+      },
+      "shape":{
+        "type":"circle",
+        "stroke":{
+          "width":1,
+          "color":"#ffffff"
+        },
+        "image":{
+          "src":"../public/assets/images/favicon.ico"
+        }
+      },
+      "opacity":{
+        "value":0.5,
+        "random":true,
+        "anim":{
+          "enable":false,
+          "speed":1
+        }
+      },
+      "size":{
+        "value": 5,
+        "random":false,
+        "anim":{
+          "enable": false,
+          "speed":30
+        }
+      },
+      "line_linked":{
+        "enable": true,
+        "distance": 120,
+        "color":"#3fff04",
+        "width":1
+      },
+      "move":{
+        "enable":true,
+        "speed":2,
+        "direction":"none",
+        "straight":false
+      }
+    },
+    "interactivity":{
+      "events":{
+        "onhover":{
+          "enable":true,
+          "mode":"bubble"
+        },
+        "onclick":{
+          "enable": true,
+          "mode":"push"
+        }
+      },
+      "modes":{
+        "repulse":{
+          "distance":50,
+          "duration":0.4
+        },
+        "bubble":{
+          "distance":70,
+          "size":18
+        }
+      }
+    }
+  }
+); // end of json
