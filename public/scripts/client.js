e966c24c0a53c5b3f9a666585bcfbd002b7bc680
Chris Stanton <cstanton0760@yahoo.com>
2017-08-06 21:36:11 -0500

    added css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,12 @@ console.log("client.js loaded...");
 
 particlesJS.load('particles-js', '../particles.json', function(){
   console.log('particles.json loaded...');
+  console.log(particles.number);
 });
+
+
+
+function link_color(){
+  var color = document.getElementById('select').value;
+  console.log(color);
+};
